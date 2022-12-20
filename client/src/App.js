d6be6e8683b7b9af821b65f1d51cef797d05b478
Alana <halo2382001@yahoo.com>
2022-12-20 10:42:19 -0500

    working on client rows

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      
+    }
+  }
+
+})
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
